32a63aed7f25517aa7307adb9a553464624c2a8c
lizidong <1945907204@qq.com>
2018-03-09 15:12:58 +0800

    Initial Commit

---
+++ ImageBrowsingImitateWeChatTests/ImageBrowsingImitateWeChatTests.m
@@ -0,0 +1,39 @@
+//
+//  ImageBrowsingImitateWeChatTests.m
+//  ImageBrowsingImitateWeChatTests
+//
+//  Created by 李子栋 on 2018/3/9.
+//  Copyright © 2018年 李子栋. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ImageBrowsingImitateWeChatTests : XCTestCase
+
+@end
+
+@implementation ImageBrowsingImitateWeChatTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
